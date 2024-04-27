4d734c0f5e726f085157159c8cc1b6ceb831fbd8
edenlisk <nkumbuyenock@gmail.com>
2024-04-27 22:42:17 +0200

    Add custom not found component

---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,8 @@
+
+export default async function SnippetNotFound() {
+    return (
+        <div>
+            <h1 className="text-xl font-bold">Sorry, We couldn&apos;t find particular snippet</h1>
+        </div>
+    )
+}
